335feffbf0454b3af2cf8d288eacc1fbacf49f90
Austin Flatt <austindflatt@gmail.com>
2021-09-20 14:35:01 -0400

    finished distance.js task

---
+++ distance.js
@@ -0,0 +1,7 @@
+let x1 = 3;
+let y1 = 10;
+let x2 = 7;
+let y2 = 14;
+let distance = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+
+console.log("The distance between (x1, y1) and (x2, y2) is "+distance+"");
